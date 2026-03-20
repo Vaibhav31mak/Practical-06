a8e359b8a7bcf97b0484e85e94eaf77c35b0a644
xyz <vaibhav.makwana@simformsolutions.com>
2026-03-20 17:44:02 +0530

    Initialized Solution

---
+++ Events/ProcessBusinessLogic.cs
@@ -0,0 +1,24 @@
+﻿namespace Events;
+
+internal class ProcessBusinessLogic
+{
+    public Action ProcessCompleted;
+
+    // Start Process - To invoke the onProcessCompleted() method.
+    // Return Type - void
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started");
+        OnProcessCompleted();
+    }
+    // Start Process - To invoke the ProcessCompleted Action.
+    // Return Type - void
+    protected virtual void OnProcessCompleted() 
+
+    {
+        //if ProcessCompleted is not null then calling the delegate
+        ProcessCompleted?.Invoke();
+    }
+
+
+}
